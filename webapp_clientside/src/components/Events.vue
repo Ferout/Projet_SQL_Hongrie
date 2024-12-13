6252cf7bf8fa7c5cee8c5741f12dc1146518081a
AuAu07 <aurelien.feracci@efrei.net>
2024-12-13 11:18:16 +0100

    header events

--- webapp_clientside/src/components/Events.vue
+++ webapp_clientside/src/components/Events.vue
@@ -221,6 +221,49 @@ export default {
 
 <style scoped>
 
+.header {
+  background-color: #42b883;
+  color: white;
+  padding: 5px 10px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  width: 95%;
+  align-items: center;
+}
+
+.nav-buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+}
+
+.nav-buttons button {
+  background-color: #42b883;
+  color: white;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background-color 0.3s;
+}
+
+.nav-buttons button:hover {
+  background-color: #36a76e;
+}
+
 .events-list {
   display: flex;
   flex-wrap: wrap;
